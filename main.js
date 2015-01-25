cbabc7653ec3ce6d445c0452dcd181a8567c3bba
mcclure15 <m001010111@gmail.com>
2015-01-25 14:10:06 -0800

    start JS file

--- main.js
+++ main.js
@@ -1,33 +1,28 @@
 $(document).ready(function() {
 
-	function Person(fullName, age, isChild, sex, socialClass, boardedLifeboat, survivedEvent) {
-		this.fullName 	 		 = fullName;
-		this.age						 = age;
-		this.isChild         = isChild;
-		this.sex 			   		 = sex;
-		this.socialClass 		 = socialClass;
-		this.boardedLifeboat = boardedLifeboat;
-		this.survivedEvent	 = survivedEvent;
+	function Person(firstName, lastName, birthYear, deathYear, ageAtDeath) {
+		this.firstName	 		 = firstName;
+		this.lastName				 = lastName;
+		this.birthYear       = birthYear;
+		this.deathYear			 = deathYear;
+		this.ageAtDeath		   = ageAtDeath;
 	}
 
 	//could i make a prototype for deciding whether or not each person is child or adult?
 
-	var person1 = new Person("Johnson, Tom", 		 35, false,   "male", 1,  true,  true),
-			person2 = new Person("Bailey, Beetle", 	 24, false,   "male", 2, false, false),
-			person3 = new Person("Lehrer, Tom", 		 67, false,   "male", 3, false, false),
-			person4 = new Person("Goulet, Robert", 	 52, false,   "male", 4,  true,  true),
-			person5 = new Person("Mercury, Freddie", 31, false,   "male", 1,  true,  true),
-			person6 = new Person("Henderson, Harry",  4,  true,   "male", 3, false, false),
-			person7 = new Person("Franklin, Sheila",  8,  true, "female", 2,  true,  true),
-			person8 = new Person("Martin, Mary",  	 62, false, "female", 1,  true, false),
-			person9 = new Person("Thomas, Frida",     2,  true, "female", 1,  true, false),
-			person10 = new Person("Bowles, Sally",   26, false, "female", 3, false, false);
+	var barber    	 = new Person("Samuel",  "Barber",       1910, 1981, 80),
+			bernstein 	 = new Person("Leonard", "Bernstein",    1918, 1990, 81),
+			cage				 = new Person("John",    "Cage",         1912, 1992, 79),
+			copland	 		 = new Person("Aaron",   "Copland",      1900, 1990, 90),
+			gershwin 		 = new Person("George",  "Gershwin",     1898, 1937, 38),
+			ives 		  	 = new Person("Charles", "Ives",         1874, 1954, 79),
+			shostakovich = new Person("Dmitri",  "Shostakovich", 1906, 1975, 69),
+			stravinsky	 = new Person("Igor",  	 "Stravinsky",   1882, 1971, 89);
 
-//	console.log(person1.age, person2.fullName);
+	var composers = [barber, bernstein, cage, copland, gershwin, ives, shostakovich, stravinsky];
 
-for(var prop in person1) {
-	console.log("person1." + prop + " = " + person1[prop]);
-}
+	
+
 
 
 
